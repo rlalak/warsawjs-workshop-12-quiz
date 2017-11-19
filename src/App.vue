9e7ed82bc4d6d2b41b6edbf3a9f65d16b0a150a0
Radosław Lalak <radek0410@gmail.com>
2017-11-19 12:32:29 +0100

    quiz details component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,44 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <the-header></the-header>
+
+    <quiz-details :quiz="quizJson"></quiz-details>
+
     <router-view/>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+  import TheHeader from './components/the-header.vue';
+  import QuizDetails from './components/quiz-details.vue';
+
+  export default {
+    name: 'app',
+    components: {
+      TheHeader, QuizDetails
+    },
+    data() {
+      return {
+        quizJson: null
+      }
+    },
+    mounted() {
+      fetch('/static/quiz.json')
+        .then((res) => res.json())
+        .then((quiz) => {
+          this.quizJson = quiz;
+        })
+    }
+  }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/the-header.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <h1>Quiz</h1>
+    <p>
+      Wybierz pytanie
+    </p>
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "TheHeader"
+  }
+</script>
+
+<style scoped>
+  h1 {
+    background-color: red;
+  }
+</style>
