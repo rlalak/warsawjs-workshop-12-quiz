7e7d5baee89fe6f1fb87213a6ba8875608b089f4
Radosław Lalak <radek0410@gmail.com>
2017-11-19 12:29:21 +0100

    quiz details component

---
+++ src/components/quiz-details.vue
@@ -0,0 +1,18 @@
+<template>
+  <section v-if="quiz">
+    <h2>{{ quiz.name }}</h2>
+    <div class="tag">
+      <p>{{ quiz.categoryName }}</p>
+    </div>
+    <img :src="quiz.promo">
+  </section>
+</template>
+
+<script>
+  export default {
+    name: "QuizDetails",
+    props: {
+      quiz: Object
+    }
+  }
+</script>
